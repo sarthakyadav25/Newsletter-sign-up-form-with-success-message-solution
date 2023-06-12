6285eccdd538e4223472f68537add1b13e3854de
Sarthak Yadav <sarthakvyadav@gmail.com>
2023-06-13 02:58:49 +0530

    Basic Working Prototype

---
+++ main.js
@@ -0,0 +1,23 @@
+function submit(){
+    email = document.getElementById('inp').value;
+     let regex = new RegExp('^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$');
+     if (regex.test(email)){
+        window.open("/success.html")
+     }
+     else{
+         const cont = document.getElementById('email');
+         const field = document.getElementById('inp')
+         field.style = "border:2px solid red";
+         const valid = document.createElement("h6");
+         valid.append("valid email address required")
+         valid.style = "color:red";
+         cont.appendChild(valid);
+     }
+ }
+
+
+//Main code
+
+let arr = document.getElementsByClassName("btn");
+const button = Array.from(arr)[0];
+button.addEventListener('click',submit);
